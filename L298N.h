96586d8e8469e3543eeaac94f57a40b54185d894
Chick92 <benjamin.bird4392@gmail.com>
2021-07-02 22:47:17 +0000

    working

---
+++ L298N.h
@@ -0,0 +1,21 @@
+/*
+  Morse.h - Library for flashing Morse code.
+  Created by David A. Mellis, November 2, 2007.
+  Released into the public domain.
+*/
+#ifndef L298N_h
+#define L298N_h
+
+#include "Arduino.h"
+
+class L298N
+{
+  public:
+    L298N(char IN1, char IN2, char EN);
+    void forwards(char pwm);
+    void backwards(char pwm);
+  private:
+    char _IN1, _IN2, _EN;
+};
+
+#endif
---
+++ examples/L298N_test/L298N_test.ino
@@ -0,0 +1,25 @@
+#include <L298N.h>
+
+char A_IN1 = 8;
+char A_IN2 = 9;
+char A_EN = 17;
+char B_IN1 = 10;
+char B_IN2 = 11;
+char B_EN = 16;
+
+L298N left(A_IN1, A_IN2, A_EN);
+L298N right(B_IN1, B_IN2, B_EN);
+
+void setup()
+{
+}
+
+void loop()
+{
+  left.forwards(255);
+  right.forwards(255);
+  delay(3000);
+  left.backwards(255);
+  right.backwards(255);
+  delay(3000);
+}
