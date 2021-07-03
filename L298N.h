2b36243137faa5e78651e03b87e1985641670fb4
Ben <Chick92@users.noreply.github.com>
2021-07-03 17:43:44 +0100

    Added velocity control (RPM)
    Added PID and interrupt encoder support to allow RPM to be set instead
    of PWM

--- L298N.h
+++ L298N.h
@@ -7,15 +7,28 @@
 #define L298N_h
 
 #include "Arduino.h"
+#include <RotaryEncoder.h>
+#include <PID_v1.h>
 
 class L298N
 {
   public:
-    L298N(char IN1, char IN2, char EN);
-    void forwards(char pwm);
-    void backwards(char pwm);
+    L298N(char IN1, char IN2, char EN, char INT1, char INT2);
+	void set_rpm(int rpm);
+	
   private:
-    char _IN1, _IN2, _EN;
+    char _IN1, _IN2, _EN, _INT1, _INT2;
+	double Input, Output, Setpoint, Kp, Ki, Kd;
+	double last_time, tick_time;
+	long old_position;
+	
+	RotaryEncoder encoder(_INT1, _INT2, RotaryEncoder::LatchMode::TWO03);
+	PID PID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+	
+	void checkPosition();
+	double calculate_rpm();
+	void forwards(char pwm);
+	void backwards(char pwm);
 };
 
 #endif
